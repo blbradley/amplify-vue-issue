d06316c56892c61670b88d6495f61200db41a703
Brandon Bradley <bbradley@iprosystems.com>
2019-02-13 14:06:10 -0600

    changes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import { ServiceWorker } from 'aws-amplify'
+const myServiceWorker = new ServiceWorker()
+
+console.log(myServiceWorker)
+
+ServiceWorker = this.serviceWorker.register('/service-worker.js', '/');
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
